81acf1830db4e7b40587b6127554d84e4c803e68
Luiz Geraldo Pinheiro <luizpinheiro.rs@gmail.com>
2020-04-16 20:45:53 -0300

    Config Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,23 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '2b113b29da565f',
+    pass: 'af8ddfc4ec028b',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/**
+ * Examples of good emails
+ * Amazonn SES
+ * Mailgun
+ * Sparkpost
+ * Mandril (Mailchimp)
+ * GMail (Block)
+ *
+ * -> Mailtrap (DEV)
+ */
