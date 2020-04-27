96b07e0b1ec40ba9a6fb792b5ce43b95a3f42fcb
Luiz Geraldo Pinheiro <luizpinheiro.rs@gmail.com>
2020-04-27 19:02:10 -0300

    Final adjusts

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import Youch from 'youch';
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
@@ -23,6 +24,12 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
+    /* this.server.use(
+      cors({
+        origin: 'https://barber.com.br',
+      })
+    ); */
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
